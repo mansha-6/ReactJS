cbe7dbb35f3130cb7ef2565ba99b85f21482811a
mansha-6 <manshasoni23@gmail.com>
2025-02-11 18:26:57 +0530

    files added

---
+++ task4/Assignment4/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import React from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
