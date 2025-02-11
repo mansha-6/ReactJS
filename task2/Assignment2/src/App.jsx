1b7a8fad5cfbfbd67a20616bebb20bbdf061a775
mansha-6 <manshasoni23@gmail.com>
2025-02-11 17:40:26 +0530

    files added

---
+++ task2/Assignment2/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Greetings from "./components/Greet"
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <Greetings/>
+    </>
+  )
+}
+
+export default App;
